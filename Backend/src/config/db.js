08d38fc1ca7e92b866a33258fdf19c2c5937ee74
Litbenyii <diazb9737@gmail.com>
2026-01-03 20:18:27 -0300

    .

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -1,17 +1,18 @@
 const { Pool } = require("pg");
-require("dotenv").config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  port: 5432,
 });
 
-pool.on("error", (err) => {
-  console.error("❌ Error inesperado en PostgreSQL:", err);
-  process.exit(1);
-});
+pool.connect()
+  .then(() => console.log("✅ Conectado a PostgreSQL"))
+  .catch(err => {
+    console.error("❌ Error conexión BD", err);
+    process.exit(1);
+  });
 
 module.exports = pool;
